f15e348a01d5bf196f3e9315b1c36132e4557b06
Narayanasetti Srinivasarao <narayanasetti.s@LAP0001419.local>
2019-11-09 12:11:42 +0530

    Adding atm-machine files

---
+++ atm-machine/precache-manifest.5e56c5838c7efcf962d619d3639a6023.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8751622bb25dfc1930aa1004104974d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2765d842578e760c485c",
+    "url": "/static/css/main.70699784.chunk.css"
+  },
+  {
+    "revision": "41d768ca66bc248db873",
+    "url": "/static/js/2.ca8a883c.chunk.js"
+  },
+  {
+    "revision": "2765d842578e760c485c",
+    "url": "/static/js/main.9068b152.chunk.js"
+  },
+  {
+    "revision": "851a9670455b7f4526b3",
+    "url": "/static/js/runtime-main.aa8d780c.js"
+  }
+]);
